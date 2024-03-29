16a67b6352aa839fa87faab00dae57893f204137
muhamejanov11 <muhamejanov.n@gmail.com>
2024-03-29 21:50:18 +0500

    done

---
+++ main.js
@@ -0,0 +1,6 @@
+let str = prompt('Napishite string informasiyu')
+let num = prompt('Napishite number informasiyu')
+let bol = prompt('Napishite boolean informasiyu')
+
+alert(`Vash string: ${str} \nvash number: ${num} \nvash boolean: ${bol}`)
+console.log(`Vash string: ${str} \nvash number: ${num} \nvash boolean: ${bol}`)
